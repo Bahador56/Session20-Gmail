ebcab55ca241950f1e6b533ca8ee6cf2112a2a5b
bahador zare <bahador567@gmail.com>
2022-03-13 05:13:00 -0700

    Gmail

---
+++ Session20-Gmail/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System;
+
+
+namespace Session20_Gmail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient smtpServer = new SmtpClient("smtp.gamil.com");
+
+
+
+            mail.From = new MailAddress("Bahador567@gmail.com");
+            mail.To.Add("Bahador567@gmail.com");
+            mail.Subject = "سلام چطوری";
+            mail.Body = "من خیلی خفنم";
+            smtpServer.Port = 587;
+            smtpServer.Credentials = new System.Net.NetworkCredential ("Bahador567@gmail.com","Pasword");
+            smtpServer.EnableSsl = true;
+
+            smtpServer.Send(mail);
+
+
+        }
+    }
+}
